33b0dc2d6e651f6919df59ef41d27f071df1a052
Tommaso Langiano <103174436+TommyLangiano@users.noreply.github.com>
2024-12-20 12:15:00 +0100

    Delete Scansphere directory

--- Scansphere/scanner/icmp_tester.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import IP, ICMP, sr1
-
-def icmp_ping(host):
-    # Invia un pacchetto ping ICMP all'host specificato
-    pkt = IP(dst=host)/ICMP()
-    response = sr1(pkt, timeout=1, verbose=False)
-    return response is not None
-
